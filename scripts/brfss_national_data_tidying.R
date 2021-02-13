8dd2bd927fabc6b10a25d662d3dbd767c5aed80d
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-02-12 17:06:06 -0700

    changed variables to factors, recoded factors

--- scripts/brfss_national_data_tidying.R
+++ scripts/brfss_national_data_tidying.R
@@ -42,7 +42,107 @@ brfss_2014 <- brfss_2014 %>%
          "X_RACE", #1 NHW, 2 NHB, 3 NHAIAN, 4 NHA, 5 HAWAIIAN, 6 NH OTHER, 7 NH MULTI, 8 HISP, 9 UNKNOWN REFUSED
          "SEX" #1 MALE 2 FEMALE
          ) %>%
-  as_tibble()
+  as_tibble() %>%
+  mutate(X_STATE = as_factor(X_STATE),
+         HADMAM = as_factor(HADMAM),
+         HOWLONG = as_factor(HOWLONG),
+         HADPAP2 = as_factor(HADPAP2),
+         LASTPAP2 = as_factor(LASTPAP2),
+         HPVTEST = as_factor(HPVTEST),
+         HPLSTTST = as_factor(HPLSTTST),
+         HADHYST2 = as_factor(HADHYST2),
+         BLDSTOOL = as_factor(BLDSTOOL),
+         LSTBLDS3 = as_factor(LSTBLDS3),
+         HADSIGM3 = as_factor(HADSIGM3),
+         HADSGCO1 = as_factor(HADSGCO1),
+         LASTSIG3 = as_factor(LASTSIG3),
+         X_AGE65YR = as_factor(X_AGE65YR),
+         X_RACE = as_factor(X_RACE),
+         SEX = as_factor(SEX),
+         YEAR = 2014)
+
+brfss_2014 <- brfss_2014 %>%
+  mutate(HADMAM = fct_recode(HADMAM,
+                             "Yes" = "1",
+                             "No" = "2",
+                             "Unknown" = "7",
+                             "Refused" = "9"),
+    HADPAP2 = fct_recode(HADPAP2,
+                              "Yes" = "1",
+                              "No" = "2",
+                              "Unknown" = "7",
+                              "Refused" = "9"),
+    HPVTEST = fct_recode(HPVTEST,
+                         "Yes" = "1",
+                         "No" = "2",
+                         "Unknown" = "7",
+                         "Refused" = "9"),
+    HADHYST2 = fct_recode(HADHYST2,
+                         "Yes" = "1",
+                         "No" = "2",
+                         "Unknown" = "7",
+                         "Refused" = "9"),
+    BLDSTOOL = fct_recode(BLDSTOOL,
+                         "Yes" = "1",
+                         "No" = "2",
+                         "Unknown" = "7",
+                         "Refused" = "9"),
+    HADSIGM3 = fct_recode(HADSIGM3,
+                          "Yes" = "1",
+                          "No" = "2",
+                          "Unknown" = "7",
+                          "Refused" = "9"),
+    X_AGE65YR = fct_recode(X_AGE65YR,
+                           "18-64" = "1",
+                           ">=65" = "2",
+                           "Unknown/Refused" ="3"),
+    X_RACE = fct_recode(X_RACE,
+                        "NHW" = "1",
+                        "NHB" = "2",
+                        "AIAN" = "3",
+                        "Asian" = "4",
+                        "Hawaiian" = "5",
+                        "Other" = "6",
+                        "Multi" = "7",
+                        "Hispanic" = "8",
+                        "Unknown/Refused" = "9"),
+    SEX = fct_recode(SEX,
+                          "Male" = "1",
+                          "Female" = "2"))
+
+# save to disk
+write_rds(brfss_2014, "data/tidy/brfss_usa_2014.RDS")
+
+# cervical cancer brfss + demographics 
+brfss_2014_cervical <- brfss_2014 %>% 
+  select(X_STATE,
+         9:13,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2014_cervical, "data/tidy/brfss_usa_2014_cervical.RDS")
+
+# breast cancer brfss + demographics 
+brfss_2014_breast <- brfss_2014 %>% 
+  select(X_STATE,
+         7:8,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2014_breast, "data/tidy/brfss_usa_2014_breast.RDS")
+
+# colorectal cancer brfss + demographics 
+brfss_2014_colorectal <- brfss_2014 %>% 
+  select(X_STATE,
+         14:18,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2014_colorectal, "data/tidy/brfss_usa_2014_colorectal.RDS")
+
+# filter to arizona only
+brfss_2014_az <- brfss_2014 %>%
+  filter(X_STATE == "4")
 
 # 2016 dataset ----
 # unzip 
@@ -79,7 +179,107 @@ brfss_2016 <- brfss_2016 %>%
          "X_RACE", #1 NHW, 2 NHB, 3 NHAIAN, 4 NHA, 5 HAWAIIAN, 6 NH OTHER, 7 NH MULTI, 8 HISP, 9 UNKNOWN REFUSED
          "SEX" #1 MALE 2 FEMALE
   ) %>%
-  as_tibble()
+  as_tibble() %>%
+  mutate(X_STATE = as_factor(X_STATE),
+         HADMAM = as_factor(HADMAM),
+         HOWLONG = as_factor(HOWLONG),
+         HADPAP2 = as_factor(HADPAP2),
+         LASTPAP2 = as_factor(LASTPAP2),
+         HPVTEST = as_factor(HPVTEST),
+         HPLSTTST = as_factor(HPLSTTST),
+         HADHYST2 = as_factor(HADHYST2),
+         BLDSTOOL = as_factor(BLDSTOOL),
+         LSTBLDS3 = as_factor(LSTBLDS3),
+         HADSIGM3 = as_factor(HADSIGM3),
+         HADSGCO1 = as_factor(HADSGCO1),
+         LASTSIG3 = as_factor(LASTSIG3),
+         X_AGE65YR = as_factor(X_AGE65YR),
+         X_RACE = as_factor(X_RACE),
+         SEX = as_factor(SEX),
+         YEAR = 2016)
+
+brfss_2016 <- brfss_2016 %>%
+  mutate(HADMAM = fct_recode(HADMAM,
+                             "Yes" = "1",
+                             "No" = "2",
+                             "Unknown" = "7",
+                             "Refused" = "9"),
+         HADPAP2 = fct_recode(HADPAP2,
+                              "Yes" = "1",
+                              "No" = "2",
+                              "Unknown" = "7",
+                              "Refused" = "9"),
+         HPVTEST = fct_recode(HPVTEST,
+                              "Yes" = "1",
+                              "No" = "2",
+                              "Unknown" = "7",
+                              "Refused" = "9"),
+         HADHYST2 = fct_recode(HADHYST2,
+                               "Yes" = "1",
+                               "No" = "2",
+                               "Unknown" = "7",
+                               "Refused" = "9"),
+         BLDSTOOL = fct_recode(BLDSTOOL,
+                               "Yes" = "1",
+                               "No" = "2",
+                               "Unknown" = "7",
+                               "Refused" = "9"),
+         HADSIGM3 = fct_recode(HADSIGM3,
+                               "Yes" = "1",
+                               "No" = "2",
+                               "Unknown" = "7",
+                               "Refused" = "9"),
+         X_AGE65YR = fct_recode(X_AGE65YR,
+                                "18-64" = "1",
+                                ">=65" = "2",
+                                "Unknown/Refused" ="3"),
+         X_RACE = fct_recode(X_RACE,
+                             "NHW" = "1",
+                             "NHB" = "2",
+                             "AIAN" = "3",
+                             "Asian" = "4",
+                             "Hawaiian" = "5",
+                             "Other" = "6",
+                             "Multi" = "7",
+                             "Hispanic" = "8",
+                             "Unknown/Refused" = "9"),
+         SEX = fct_recode(SEX,
+                          "Male" = "1",
+                          "Female" = "2"))
+
+# save to disk
+write_rds(brfss_2016, "data/tidy/brfss_usa_2016.RDS")
+
+# cervical cancer brfss + demographics 
+brfss_2016_cervical <- brfss_2016 %>% 
+  select(X_STATE,
+         9:13,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2016_cervical, "data/tidy/brfss_usa_2016_cervical.RDS")
+
+# breast cancer brfss + demographics 
+brfss_2016_breast <- brfss_2016 %>% 
+  select(X_STATE,
+         7:8,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2016_breast, "data/tidy/brfss_usa_2016_breast.RDS")
+
+# colorectal cancer brfss + demographics 
+brfss_2016_colorectal <- brfss_2016 %>% 
+  select(X_STATE,
+         14:18,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2016_colorectal, "data/tidy/brfss_usa_2016_colorectal.RDS")
+
+# filter to arizona only
+brfss_2016_az <- brfss_2016 %>%
+  filter(X_STATE == "4")
 
 # 2018 dataset ----
 # unzip 
@@ -114,13 +314,115 @@ brfss_2018 <- brfss_2018 %>%
          "LASTSIG3", #1 <12 months, 2 <24months, 3 <3years, 4 3-5years, 5 5-10 years, 6 >10years, 7 unknown, 9 refused
          "X_AGE65YR", #1 age 18-64, 2 age >= 65, 3 unknown refused missing
          "X_RACE", #1 NHW, 2 NHB, 3 NHAIAN, 4 NHA, 5 HAWAIIAN, 6 NH OTHER, 7 NH MULTI, 8 HISP, 9 UNKNOWN REFUSED
-         "SEX" = "SEX1" #1 MALE, 2 FEMALE, 3 
+         "SEX" = "SEX1" #1 MALE, 2 FEMALE, 7 UNKNOWN, 9 REFUSED
   ) %>%
-  as_tibble()
+  as_tibble() %>%
+  mutate(X_STATE = as_factor(X_STATE),
+         HADMAM = as_factor(HADMAM),
+         HOWLONG = as_factor(HOWLONG),
+         HADPAP2 = as_factor(HADPAP2),
+         LASTPAP2 = as_factor(LASTPAP2),
+         HPVTEST = as_factor(HPVTEST),
+         HPLSTTST = as_factor(HPLSTTST),
+         HADHYST2 = as_factor(HADHYST2),
+         BLDSTOOL = as_factor(BLDSTOOL),
+         LSTBLDS3 = as_factor(LSTBLDS3),
+         HADSIGM3 = as_factor(HADSIGM3),
+         HADSGCO1 = as_factor(HADSGCO1),
+         LASTSIG3 = as_factor(LASTSIG3),
+         X_AGE65YR = as_factor(X_AGE65YR),
+         X_RACE = as_factor(X_RACE),
+         SEX = as_factor(SEX),
+         YEAR = 2018)
 
-# combine all datasets together
-brfss <- brfss_2014 %>%
-  full_join(brfss_2016) %>%
-  full_join(brfss_2018) 
+brfss_2018 <- brfss_2018 %>%
+  mutate(HADMAM = fct_recode(HADMAM,
+                             "Yes" = "1",
+                             "No" = "2",
+                             "Unknown" = "7",
+                             "Refused" = "9"),
+         HADPAP2 = fct_recode(HADPAP2,
+                              "Yes" = "1",
+                              "No" = "2",
+                              "Unknown" = "7",
+                              "Refused" = "9"),
+         HPVTEST = fct_recode(HPVTEST,
+                              "Yes" = "1",
+                              "No" = "2",
+                              "Unknown" = "7",
+                              "Refused" = "9"),
+         HADHYST2 = fct_recode(HADHYST2,
+                               "Yes" = "1",
+                               "No" = "2",
+                               "Unknown" = "7",
+                               "Refused" = "9"),
+         BLDSTOOL = fct_recode(BLDSTOOL,
+                               "Yes" = "1",
+                               "No" = "2",
+                               "Unknown" = "7",
+                               "Refused" = "9"),
+         HADSIGM3 = fct_recode(HADSIGM3,
+                               "Yes" = "1",
+                               "No" = "2",
+                               "Unknown" = "7",
+                               "Refused" = "9"),
+         X_AGE65YR = fct_recode(X_AGE65YR,
+                                "18-64" = "1",
+                                ">=65" = "2",
+                                "Unknown/Refused" ="3"),
+         X_RACE = fct_recode(X_RACE,
+                             "NHW" = "1",
+                             "NHB" = "2",
+                             "AIAN" = "3",
+                             "Asian" = "4",
+                             "Hawaiian" = "5",
+                             "Other" = "6",
+                             "Multi" = "7",
+                             "Hispanic" = "8",
+                             "Unknown/Refused" = "9"),
+         SEX = fct_recode(SEX,
+                          "Male" = "1",
+                          "Female" = "2"))
 
-brfss <- as_tibble(brfss)
+# save to disk
+write_rds(brfss_2018, "data/tidy/brfss_usa_2018.RDS")
+
+# cervical cancer brfss + demographics 
+brfss_2018_cervical <- brfss_2018 %>% 
+  select(X_STATE,
+         9:13,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2018_cervical, "data/tidy/brfss_usa_2018_cervical.RDS")
+
+# breast cancer brfss + demographics 
+brfss_2018_breast <- brfss_2018 %>% 
+  select(X_STATE,
+         7:8,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2018_breast, "data/tidy/brfss_usa_2018_breast.RDS")
+
+# colorectal cancer brfss + demographics 
+brfss_2018_colorectal <- brfss_2018 %>% 
+  select(X_STATE,
+         14:18,
+         19:22)
+
+# save to RDS 
+write_rds(brfss_2018_colorectal, "data/tidy/brfss_usa_2018_colorectal.RDS")
+
+# filter to arizona only
+brfss_2018_az <- brfss_2018 %>%
+  filter(X_STATE == "4")
+
+# combine all brfss az 2014-18
+brfss_az <- brfss_2014_az %>%
+  full_join(brfss_2016_az) %>%
+  full_join(brfss_2018_az)
+
+# save to disk
+write_rds(brfss_az, "data/tidy/brfss_az.RDS")
+
