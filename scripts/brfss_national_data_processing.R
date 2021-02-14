33b8edd8f61ce32d5d105585fed1a994f527894a
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-02-13 19:24:52 -0700

    exploratory data analysis for national datesets

---
+++ scripts/brfss_national_data_processing.R
@@ -0,0 +1,269 @@
+# tidy data from brfss export
+# set up
+# load packages ----
+library(here)
+library(tidyverse)
+library(ggthemes)
+
+# read data ----
+brfss_breast <- read_rds("data/tidy/brfss_usa_breast.RDS")
+brfss_cervical <- read_rds("data/tidy/brfss_usa_cervical.RDS")
+brfss_colorectal <- read_rds("data/tidy/brfss_usa_cervical.RDS")
+
+# breast cancer screening ----
+# plot proportion had mammogram over time by race
+# only for non hispanic white, american indian, and hispanic
+brfss_breast %>%
+  drop_na(HADMAM) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX == "Female") %>%
+  group_by(YEAR, X_RACE) %>%
+  count(HADMAM) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HADMAM)) +
+  geom_point() +
+  geom_line() +
+  geom_hline(aes(yintercept = 0.811, color = "2020 Objective")) +
+  facet_wrap(~X_RACE) +
+  theme_bw() + 
+  labs(title = "Breast Cancer Screening by Race / Ethnicity",
+       subtitle = "AZ: Have You Ever Had a Mammogram?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# plot proportion had mammogram over time by age
+# only for non hispanic white, american indian, and hispanic
+brfss_breast %>%
+  drop_na(HADMAM) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX == "Female",
+         X_AGE65YR %in% c("18-64", ">=65")) %>%
+  group_by(YEAR, X_AGE65YR) %>%
+  count(HADMAM) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HADMAM)) +
+  geom_point() +
+  geom_line() +
+  geom_hline(aes(yintercept = 0.811, color = "2020 Objective")) +
+  facet_wrap(~X_AGE65YR) +
+  theme_bw() + 
+  labs(title = "Breast Cancer Screening by Age Group",
+       subtitle = "AZ: Have You Ever Had a Mammogram?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# cervical cancer screening ---- 
+# proportion had pap test ----
+# by race 
+# only for non hispanic white, american indian, and hispanic
+brfss_cervical %>%
+  drop_na(HADPAP2) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX == "Female") %>%
+  group_by(YEAR, X_RACE) %>%
+  count(HADPAP2) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HADPAP2)) +
+  geom_point() +
+  geom_line() +
+  geom_hline(aes(yintercept = 0.93, color = "2020 Objective")) +
+  facet_wrap(~X_RACE) +
+  theme_bw() + 
+  labs(title = "Cervical Cancer Screening by Race / Ethnicity",
+       subtitle = "AZ: Have You Ever Had a Pap Test?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# plot proportion had pap test over time by age
+# only for non hispanic white, american indian, and hispanic
+brfss_cervical %>%
+  drop_na(HADPAP2) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX == "Female",
+         X_AGE65YR %in% c("18-64", ">=65")) %>%
+  group_by(YEAR, X_AGE65YR) %>%
+  count(HADPAP2) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HADPAP2)) +
+  geom_point() +
+  geom_line() +
+  geom_hline(aes(yintercept = 0.93, color = "2020 Objective")) +
+  facet_wrap(~X_AGE65YR) +
+  theme_bw() + 
+  labs(title = "Cervical Cancer Screening by Age Group",
+       subtitle = "AZ: Have You Ever Had a Pap Test?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# proportion had hpv test ----
+# by race 
+# only for non hispanic white, american indian, and hispanic
+brfss_cervical %>%
+  drop_na(HPVTEST) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX == "Female") %>%
+  group_by(YEAR, X_RACE) %>%
+  count(HPVTEST) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HPVTEST)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~X_RACE) +
+  theme_bw() + 
+  labs(title = "Cervical Cancer Screening by Race / Ethnicity",
+       subtitle = "AZ: Have You Ever Had an HPV Test?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# plot proportion had hpv test over time by age
+# only for non hispanic white, american indian, and hispanic
+brfss_cervical %>%
+  drop_na(HPVTEST) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX == "Female",
+         X_AGE65YR %in% c("18-64", ">=65")) %>%
+  group_by(YEAR, X_AGE65YR) %>%
+  count(HPVTEST) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HPVTEST)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~X_AGE65YR) +
+  theme_bw() + 
+  labs(title = "Cervical Cancer Screening by Age Group",
+       subtitle = "AZ: Have You Ever Had an HPV Test?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# colorectal cancer screening ---- 
+# blood stool test ----
+# proportion by race 
+# only for non hispanic white, american indian, and hispanic
+# male and female
+brfss_colorectal %>%
+  drop_na(BLDSTOOL) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic")) %>%
+  group_by(YEAR, X_RACE) %>%
+  count(BLDSTOOL) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = BLDSTOOL)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~X_RACE) +
+  theme_bw() + 
+  labs(title = "Colorectal Cancer Screening by Race / Ethnicity",
+       subtitle = "AZ: Ever had a blood stool test?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# plot proportion had hpv test over time by age
+# only for non hispanic white, american indian, and hispanic
+brfss_colorectal %>%
+  drop_na(BLDSTOOL) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         X_AGE65YR %in% c("18-64", ">=65")) %>%
+  group_by(YEAR, X_AGE65YR) %>%
+  count(BLDSTOOL) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = BLDSTOOL)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~X_AGE65YR) +
+  theme_bw() + 
+  labs(title = "Colorectal Cancer Screening by Age Group",
+       subtitle = "AZ: Ever had a blood stool test?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# plot proportion had hpv test over time by sex
+# only for non hispanic white, american indian, and hispanic
+brfss_colorectal %>%
+  drop_na(BLDSTOOL) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX %in% c("Male", "Female")) %>%
+  group_by(YEAR, SEX) %>%
+  count(BLDSTOOL) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = BLDSTOOL)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~SEX) +
+  theme_bw() + 
+  labs(title = "Colorectal Cancer Screening by Sex",
+       subtitle = "AZ: Ever had a blood stool test?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# sigmoidoscopy colonoscopy ----
+# proportion by race 
+# only for non hispanic white, american indian, and hispanic
+# male and female
+brfss_colorectal %>%
+  drop_na(HADSIGM3) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic")) %>%
+  group_by(YEAR, X_RACE) %>%
+  count(HADSIGM3) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HADSIGM3)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~X_RACE) +
+  theme_bw() + 
+  labs(title = "Colorectal Cancer Screening by Race / Ethnicity",
+       subtitle = "AZ: Ever had a sigmoidoscopy / colonoscopy?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# plot proportion had hpv test over time by age
+# only for non hispanic white, american indian, and hispanic
+brfss_colorectal %>%
+  drop_na(HADSIGM3) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         X_AGE65YR %in% c("18-64", ">=65")) %>%
+  group_by(YEAR, X_AGE65YR) %>%
+  count(HADSIGM3) %>%
+  mutate(prop = n/sum(n)) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HADSIGM3)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~X_AGE65YR) +
+  theme_bw() + 
+  labs(title = "Colorectal Cancer Screening by Age Group",
+       subtitle = "AZ: Ever had a sigmoidoscopy / colonoscopy?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
+
+# plot proportion had hpv test over time by sex
+# only for non hispanic white, american indian, and hispanic
+brfss_colorectal %>%
+  drop_na(HADSIGM3) %>%
+  filter(X_RACE %in% c("NHW", "AIAN", "Hispanic"),
+         SEX %in% c("Male", "Female")) %>%
+  group_by(YEAR, SEX) %>%
+  count(HADSIGM3) %>%
+  mutate(prop = n/sum(n),
+         margin = qnorm(0.975)*sqrt(prop*(1-prop)/n),
+         low = prop - margin,
+         high = prop + margin) %>%
+  ggplot(mapping = aes(x = YEAR, y = prop, color = HADSIGM3)) +
+  geom_point() +
+  geom_line() +
+  geom_errorbar(mapping = aes(ymin = low, ymax = high), alpha = .75) +
+  facet_wrap(~SEX) +
+  theme_bw() + 
+  labs(title = "Colorectal Cancer Screening by Sex",
+       subtitle = "AZ: Ever had a sigmoidoscopy / colonoscopy?",
+       x = "Year",
+       y = "Proportion of Responses",
+       caption = "Source: 2014-2018 CDC BRFSS")
